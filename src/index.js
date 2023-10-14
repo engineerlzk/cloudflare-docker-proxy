fc9953c22e5f81cf233f6db36668ef6f26d47771
engineerlzk <engineerlzk@qq.com>
2023-10-14 11:45:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.e-whisper.com": "https://registry-1.docker.io",
-  "quay.e-whisper.com": "https://quay.io",
-  "gcr.e-whisper.com": "https://gcr.io",
-  "k8s-gcr.e-whisper.com": "https://k8s.gcr.io",
-  "k8s.e-whisper.com": "https://registry.k8s.io",
-  "ghcr.e-whisper.com": "https://ghcr.io",
-  "cloudsmith.e-whisper.com": "https://docker.cloudsmith.io",
+  "docker.frp2.cc": "https://registry-1.docker.io",
+  "quay.frp2.cc": "https://quay.io",
+  "gcr.frp2.cc": "https://gcr.io",
+  "k8s-gcr.frp2.cc": "https://k8s.gcr.io",
+  "k8s.frp2.cc": "https://registry.k8s.io",
+  "ghcr.frp2.cc": "https://ghcr.io",
+  "cloudsmith.frp2.cc": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
